223e0d54e77c1c7ae55bce48fd8b13c1580c0900
Chloedj <155569174+Chloedj@users.noreply.github.com>
2024-01-06 21:39:05 +0000

    Added API to update city and temperature

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
+function updateWeather(response){
+    console.log(response.data.temperature.current);
+    let temperature = Math.round(response.data.temperature.current);
+    let tempertureElement = document.querySelector("#temperature")
+    tempertureElement.innerHTML = `${temperature}`;
+    let cityElement = document.querySelector("#weather-app-city")
+cityElement.innerHTML = response.data.city;
+
+}
+
+
+
+function searchCity(city) {
+let apiKey = "ab6f607t41943e02220ae3724eo64aeb";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(updateWeather);
+}
+
+
+
+
+
 function searchSubmit(event) {
     event.preventDefault();
 let searchInput = document.querySelector("#search-form-input");
-let cityElement = document.querySelector("#weather-app-city")
-cityElement.innerHTML = searchInput.value;
+
+searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form")
 
 searchFormElement.addEventListener("submit", searchSubmit)
+
+searchCity("Paris")
